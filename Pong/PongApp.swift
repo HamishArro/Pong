0ae40edab55f6f1a246b5d598959f98144dbc762
Hamish <hamisharro@icloud.com>
2021-06-07 13:34:20 +0100

    Initial Commit

---
+++ Pong/PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongApp.swift
+//  Pong
+//
+//  Created by Hamish Arro on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
