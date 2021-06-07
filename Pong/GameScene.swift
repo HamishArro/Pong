e15bf8d14b502836ecb3aebe80d929fc61545fc2
Hamish <hamisharro@icloud.com>
2021-06-07 15:13:07 +0100

    create structure for new classes

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -14,13 +14,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate, ObservableObject {
         let border = SKPhysicsBody(edgeLoopFrom: frame)
         border.friction = 0
         physicsBody = border
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
+        
+        makeBall()
+        makePaddle()
+        makeBricks()
+        makeFloor()
     }
     
     func makeBall() {
         //
     }
     
+    func makePaddle() {
+        //
+    }
+    
     func makeBricks() {
+        //
+    }
+    
+    func makeFloor() {
         //
     }
 }
