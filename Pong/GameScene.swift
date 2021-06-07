cfc13adbb9afaf19a6f3b2702ae839cf943e72e2
Hamish <hamisharro@icloud.com>
2021-06-07 15:38:50 +0100

    complete make paddle function

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -7,7 +7,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate, ObservableObject {
     
     let ball = SKShapeNode(circleOfRadius: CGFloat(12))
     let paddle = SKSpriteNode(color: SKColor(.white), size: CGSize(width: 120, height: 16))
-    let floor = SKSpriteNode(color: SKColor(.clear), size: CGSize(width: UIScreen.main.bounds.width, height: 20))
+    let floor = SKSpriteNode(color: SKColor(.white), size: CGSize(width: UIScreen.main.bounds.width, height: 20))
     
     override func didMove(to view: SKView) {
         self.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
@@ -35,11 +35,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate, ObservableObject {
     }
     
     func makeBall() {
-        //
+        ball.name = "ball"
+        ball.fillColor = .white
+        ball.position = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 1.6)
+        ball.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 24, height: 24))
+        ball.physicsBody!.allowsRotation = false
+        ball.physicsBody!.friction = 0
+        ball.physicsBody!.restitution = 1
+        ball.physicsBody!.linearDamping = 0
+        ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
+        addChild(ball)
+        ball.physicsBody!.applyImpulse(CGVector(dx: 8, dy: -8))
     }
     
     func makePaddle() {
-        //
+        paddle.name = "paddle"
+        paddle.position = CGPoint(x: UIScreen.main.bounds.width / 2, y: 40)
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 24, height: 24))
+        paddle.physicsBody!.allowsRotation = false
+        paddle.physicsBody!.friction = 0
+        paddle.physicsBody!.restitution = 1
+        paddle.physicsBody!.isDynamic = false
+        paddle.physicsBody!.contactTestBitMask = paddle.physicsBody!.collisionBitMask
+        addChild(paddle)
     }
     
     func makeBricks() {
@@ -47,7 +65,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate, ObservableObject {
     }
     
     func makeFloor() {
-        //
+        floor.name = "floor"
+        floor.position = CGPoint(x: UIScreen.main.bounds.width / 2, y: 0)
+        floor.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 24, height: 24))
+        floor.physicsBody!.allowsRotation = false
+        floor.physicsBody!.isDynamic = false
+        floor.physicsBody!.contactTestBitMask = floor.physicsBody!.collisionBitMask
+        addChild(floor)
     }
 }
 
